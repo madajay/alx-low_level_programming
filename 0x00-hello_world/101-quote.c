6d93985a214577095a5170a3829b732697af7d77
madajay <madajay65@gmail.com>
2023-02-17 05:45:13 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - Entry Poin
+ *Description: Null for now
+ *Return: Always 1
+ */
+
+int main(void)
+{
+write(and that piece of art is useful" - Dora Korpar, 2015-10-19/n");
+return (1);
+}
