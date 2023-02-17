aaa254037890d06cdf331ae0e6f7308df540dcf9
madajay <madajay65@gmail.com>
2023-02-17 04:36:18 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@ Return: Always 0 (Success)
 */
 int main(void)
 {
-  puts("\"programming is like building a multilingual puzzle")
+  puts("\"programming is like building a multilingual puzzle");
     return (0);
    }
