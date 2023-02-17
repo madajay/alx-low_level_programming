76f90263d14d524a043f7c754f3670f831720399
madajay <madajay65@gmail.com>
2023-02-17 04:30:20 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main -Entry point
+ *Description: Print the enclosed chars
+Return: Always 0 (Success)
+*/
+int main(void)
+{
+  puts("\"programming is like building a multilingual puzzle")
+    return (0);
+   }
