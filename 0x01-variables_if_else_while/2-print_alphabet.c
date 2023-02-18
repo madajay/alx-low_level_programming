285b0e5dc764f090fdd7836c87a42a03ea62da88
madajay <madajay65@gmail.com>
2023-02-17 18:16:24 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print lowercase a-z
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char alphabet = 'a';
+
+  while (alphabet <='z')
+    {
+      putchar(alphabet);
+      alphabet++;
+    }
+  putchar('\n');
+
+  return (0);
+}
