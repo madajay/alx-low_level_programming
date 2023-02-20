669744b18837226cd34c82d230aff0bb19a2858a
madajay <madajay65@gmail.com>
2023-02-20 12:06:43 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Prints the lower alphabeth in reverse
+ *Objective - Use on putchar
+ *Return - Always 0
+ */
+int main(void)
+{
+  char a;
+  for(a="z"; z>="a"; a--)
+    {
+      putchar(a);
+    }
+  putchar("\n");
+  return 0;
+}
