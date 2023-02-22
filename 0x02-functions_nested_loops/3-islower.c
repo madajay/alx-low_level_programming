02f71deb4ddfdd8f1a51912af2ba4c930d614220
madajay <madajay65@gmail.com>
2023-02-22 03:53:55 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_islower - checks for lowercase letters
+ *@c: the character to check for lowercase letters
+ *Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('n');
+}
