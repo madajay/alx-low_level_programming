44db3caf2cd18ef74ac55751b31c968609b627cf
madajay <madajay65@gmail.com>
2023-02-21 23:31:53 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,14 @@
 
 void print_alphabet_x10(void)
 {
-	int count;
+	int count = 0;
 	char letter;
 
-	while (count = 0; count <= 9; count++)
+	while (count++ <= 9)
 	{
 
 		for (letter = 'a'; letter <= 'z'; letter++)
-		{
 			_putchar(letter);
-		}
 	_putchar('\n');
 	}
 }
