0251ab33c725c6f193368336dedeec3c9413e28a
madajay <madajay65@gmail.com>
2023-02-25 07:00:16 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,14 +9,6 @@
 
 int _isupper(int c);
 
-/**
- * _isupper - function definition
- * Description: tests case of c
- * @c: character to test
- * Return: 1 if upper 0 if not upper
- */
-
-int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
