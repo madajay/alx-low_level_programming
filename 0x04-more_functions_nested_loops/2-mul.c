1e33f6a41624aa27b889262322b1ba13fa5b510d
madajay <madajay65@gmail.com>
2023-02-26 12:24:18 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * mul - function definition
+ * Description: multiply 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the product
  */
 
-int main(void)
+int mul(int a, int b)
 {
-	printf("%d\n", mul(98, 1024));
-	printf("%d\n", mul(-402, 4096));
-	return (0);
+	int product = a * b;
+
+	return (product);
 }
