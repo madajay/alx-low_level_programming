1828932947b846342ecc91552b18e62d10f8876f
madajay <madajay65@gmail.com>
2023-02-26 23:27:39 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,13 +11,13 @@ void print_most_numbers(void)
 
 	for (number = 0; number <= 9; number++)
 	{
-		if (number == 2 || number == 4)
+		if (number != 2 || number != 4)
 		{
 			continue;
 		}
 		else
 		{
-			_putchar(number + '0');
+			_putchar((number %10) + '0');
 		}
 	}
 	_putchar('\n');
